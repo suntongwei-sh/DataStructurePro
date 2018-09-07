21526786bfd5f48e8a505c7278543b86fbef0a85
suntongwei-sh <suntongweimy@163.com>
2018-09-07 17:00:19 +0800

    ----

--- src/data/stack/Stack.java
+++ src/data/stack/Stack.java
@@ -3,14 +3,35 @@ package data.stack;
 /**
  * 栈
  * 接口,定义基本操作
+ *
  * @param <E>
  */
 public interface Stack<E> {
 
     int getSize();
+
     int getCapacity();
+
+    /**
+     * 入栈
+     *
+     * @param e
+     */
     void push(E e);
+
+    /**
+     * 出栈
+     *
+     * @return
+     */
     E pop();
+
     boolean isEmpty();
+
+    /**
+     * 查看栈顶元素
+     *
+     * @return
+     */
     E peek();
 }
